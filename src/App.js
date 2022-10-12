d5a32a6fca38d582858841a4ec53637afd1bdbca
Gabriel Wilkins <wilkigab001@gmail.com>
2022-10-12 16:10:45 -0600

    End of wednesday

--- src/App.js
+++ src/App.js
@@ -6,18 +6,32 @@ import Square from './Square';
 
 function App() {
 
-  const [squares, setSquares] = useState(["","","","","","","","","",])
+  const [squares, setSquares] = useState(["","","","","","","","",""])
   const [player, setPlayer ] = useState(true)
+
+  const handleClick = () => {
+    setSquares(['','','','','','','','',''])
+    setPlayer(true)
+  }
 
   
   return (
     <div className="App">
-      <Square 
-          squares={squares}
-          setSquares={setSquares}
-          player={player}
-          setPlayer={setPlayer}
-      />
+      <button onClick={handleClick}>Reset</button>
+      <div className="container">
+        {squares.map((val, index) => {
+          return (
+            <Square
+              setSquares={setSquares}
+              index={index}
+              squareValue={val}
+              squares={squares}
+              player={player}
+              setPlayer={setPlayer}
+            />
+          )
+        })}
+      </div>
     </div>
   );
 }
