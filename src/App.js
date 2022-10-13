aa2b06f5ce0468a04a7a85d37a7580305c3f5319
Gabriel Wilkins <wilkigab001@gmail.com>
2022-10-13 17:12:04 -0600

    Final push, may add css for fun

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import React, {useState} from 'react';
-import './App.css';
-import Square from './Square';
+import React, { useState } from "react";
 
+import "./App.css";
 
+import Square from "./Square";
 
 function App() {
-
-  const [squares, setSquares] = useState(["","","","","","","","",""])
-  const [player, setPlayer ] = useState(true)
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState(true);
 
   const handleClick = () => {
-    setSquares(['','','','','','','','',''])
-    setPlayer(true)
-  }
+    setSquares(["", "", "", "", "", "", "", "", ""]);
+    setPlayer(true);
+  };
 
-  
+  const calculateWinner = (arr) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (arr[a] === "X" && arr[b] === "X" && arr[c] === "X") {
+        return (
+          <div>
+            <p>{arr[a]} won!</p>
+          </div>
+        );
+      } else if (arr[a] === "O" && arr[b] === "O" && arr[c] === "O") {
+        return (
+          <div>
+            <p>{arr[a]} won!</p>
+          </div>
+        );
+      }
+    }
+    return "Who will win?";
+  };
+
   return (
     <div className="App">
       <button onClick={handleClick}>Reset</button>
@@ -29,9 +58,10 @@ function App() {
               player={player}
               setPlayer={setPlayer}
             />
-          )
+          );
         })}
       </div>
+      <span>{calculateWinner(squares)}</span>
     </div>
   );
 }
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,25 +1,28 @@
 import React from "react";
-import './App.css'
+import "./App.css";
 const Square = (props) => {
-
-    const handleClick = () => {
-        if(!props.squareValue ){
-            if(props.player){
-                props.squares.splice(props.index, 1, "X") 
-                props.setSquares(props.squares)
-                props.setPlayer(!props.player)
-            } else {
-                props.squares.splice(props.index, 1, "O") 
-                props.setSquares(props.squares)
-                props.setPlayer(!props.player)
-            }   
-        }
+  const handleClick = () => {
+    if (!props.squareValue) {
+      if (props.player) {
+        props.squares.splice(props.index, 1, "X");
+        props.setSquares(props.squares);
+        props.setPlayer(!props.player);
+      } else {
+        props.squares.splice(props.index, 1, "O");
+        props.setSquares(props.squares);
+        props.setPlayer(!props.player);
+      }
     }
-    return (
+  };
+  return (
     <div className="square" onClick={handleClick}>
-        {props.squareValue[props.index] === "O" ? <img src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" /> : props.squareValue}
+      {props.squareValue === "O" ? (
+        <img src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" />
+      ) : (
+        props.squareValue
+      )}
     </div>
-    );
+  );
 };
 
-export default Square
+export default Square;
